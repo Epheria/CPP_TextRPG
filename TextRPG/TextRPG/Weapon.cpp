6929cbbe9b2854032a5b5a21bf67596bd7a98f82
duddndtlsrl <duddndtlsrl@daum.net>
2021-05-11 15:59:36 +0900

    1. 몬스터 승리시 ** 승리 없애기 2. 가위바위보 enum 으로 고치기 3. 로드시 인벤토리 무기장착하고 로드되기 혹시 할 수 있으면 장착해제, 인벤토리 제한두기

--- TextRPG/TextRPG/Weapon.cpp
+++ TextRPG/TextRPG/Weapon.cpp
@@ -149,7 +149,6 @@ void WeaponManager::ShowWeaponInfo(WEAPONTYPE eType, Player& User, string Weapon
 		else if (iSelect == iSel + 3)
 			return;
 	}
-
 }
 
 void WeaponManager::ShowList(vector<Weapon*> tmp, Player& User, string WeaponTypeName, int iPage, int iMax)
@@ -184,108 +183,6 @@ void WeaponManager::ShowList(vector<Weapon*> tmp, Player& User, string WeaponTyp
 	iHeight += 3;
 	m_DrawManager.DrawMidText("나가기", WIDTH, iHeight);
 }
-
-//void WeaponManager::ShowWeaponInfo(WEAPONTYPE eType, Player& User, string WeaponTypeName, int iMax, int iIndex)
-//{
-//	int iSelect, iCur = 0;
-//	int n = 2;
-//	int iGold;
-//	int iIndex_s = iIndex;
-//
-//	while (1)
-//	{
-//		system("cls");
-//		int iHeight = HEIGHT - 30;
-//		iGold = User.GetGold();
-//		BLUE
-//			m_DrawManager.BoxDraw(START_X, START_Y, WIDTH, HEIGHT);
-//		ORIGINAL
-//			m_DrawManager.DrawMidText("보유 Gold : ", WIDTH, iHeight);
-//		cout << iGold;
-//		iHeight += n;
-//		m_DrawManager.DrawMidText(WeaponTypeName, WIDTH, iHeight);
-//		cout << " Shop";
-//		YELLOW
-//			for (int i = 0; i < iMax; i++)
-//			{
-//				if (i > 4)
-//					break;
-//				if (WeaponList[iIndex]->GetWeaponType() == eType)
-//				{
-//					WeaponList[iIndex]->ShowWeapon(n, iHeight, WeaponTypeName);
-//					iHeight += n + 1;
-//					iIndex++;
-//					iCur++;
-//				}
-//			}
-//		ORIGINAL
-//			iHeight += n;
-//		m_DrawManager.DrawMidText("이전 페이지", WIDTH, iHeight);
-//		iHeight += 3;
-//		m_DrawManager.DrawMidText("다음 페이지", WIDTH, iHeight);
-//		iHeight += 3;
-//		m_DrawManager.DrawMidText("나가기", WIDTH, iHeight);
-//		if (iMax <= 5)
-//		{
-//			iIndex -= iCur;
-//			iSelect = m_DrawManager.MenuSelectCursor(iMax + 3, 3, WIDTH / 4, HEIGHT - 26);
-//			if (iSelect <= iMax)
-//			{
-//				iIndex_s = iIndex_s - 1 + iSelect;
-//				if (iGold < WeaponList[iIndex_s]->GetPrice())
-//					continue;
-//				else
-//				{
-//					User.BuyWeapon(WeaponList[iIndex_s]->GetPrice());
-//					User.EquipWeapon(WeaponList, iIndex_s);
-//				}
-//				continue;
-//			}
-//			else if (iSelect == iMax + 1)
-//			{
-//				ShowWeaponInfo(eType, User, WeaponTypeName);
-//				return;
-//			}
-//			else if (iSelect == iMax + 2)
-//				continue;
-//			else if (iSelect == iMax + 3)
-//				return;
-//		}
-//		else // 5이상일때
-//		{
-//			iSelect = m_DrawManager.MenuSelectCursor(8, 3, WIDTH / 4, HEIGHT - 26);
-//			if (iSelect <= 5)
-//			{
-//				iIndex_s = iIndex_s - 1 + iSelect;
-//				if (iGold < WeaponList[iIndex_s]->GetPrice())
-//					continue;
-//				else
-//				{
-//					User.BuyWeapon(WeaponList[iIndex_s]->GetPrice());
-//					User.EquipWeapon(WeaponList, iIndex_s);
-//				}
-//				continue;
-//			}
-//			else if (iSelect == 5 + 1)
-//			{
-//				ShowWeaponInfo(eType, User, WeaponTypeName);
-//				return;
-//			}
-//			else if (iSelect == 5 + 2)
-//			{
-//				if (iMax > 5)
-//				{
-//					iMax -= 5;
-//				}
-//				ShowWeaponInfo(eType, User, WeaponTypeName, iMax, iIndex);
-//
-//				return;
-//			}
-//			else if (iSelect == 5 + 3)
-//				return;
-//		}
-//	}
-//}
 
 Weapon::~Weapon()
 {
